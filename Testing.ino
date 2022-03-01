c3fa01924c65e2cf32f7aba63bde361b56730661
Christine Nguyen <70961592+CNguyen-1@users.noreply.github.com>
2022-03-01 17:56:23 -0600

    Media and Tech Partner Project

---
+++ Testing.ino
@@ -0,0 +1,36 @@
+int sensorVal;
+const int ledPin = 4;
+const int sensorPin = 12;
+const int buzzerPin = 8;
+int count = 1;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+}
+
+
+void loop(){
+ 
+sensorVal = digitalRead(sensorPin);
+
+
+if (sensorVal == HIGH){
+  digitalWrite(ledPin, HIGH);
+  //digitalWrite(buzzerPin, HIGH);
+  tone(buzzerPin, 1000);
+  //delay(1000);
+}
+
+else {
+  digitalWrite(ledPin, LOW);
+  //digitalWrite(buzzerPin, LOW);
+  noTone(buzzerPin);
+  //delay(1000);
+}
+}
+
+
+    
